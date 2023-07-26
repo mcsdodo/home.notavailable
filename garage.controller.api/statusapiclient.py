50199a7c65782f34fe155275eba03867cfbe40fe
Jozef Lačný <lacny.jozef@gmail.com>
2023-07-26 23:03:06 +0200

    status.service testing on Rpi, reporting to server

--- garage.controller.api/statusapiclient.py
+++ garage.controller.api/statusapiclient.py
@@ -6,17 +6,11 @@ class StatusApiClient:
         client = section['CF-Access-Client-Id']
         secret = section['CF-Access-Client-Secret']
         self.headers = {'CF-Access-Client-Id' : client,'CF-Access-Client-Secret' : secret}
-        print(self.headers)
     def get_status(self):
         r = requests.get(self.url, headers=self.headers)
         return r
     def set_status(self, state):
         r = requests.post(self.url, headers=self.headers, json ={'state': state})
-        print(state)
         return r
     def report_health(self):
-        # https://stackoverflow.com/a/45601591/1712948
-        try:
-            requests.head(self.url, timeout=0.0000000001)
-        except requests.exceptions.ReadTimeout:
-            pass
+        requests.head(self.url, headers=self.headers)
