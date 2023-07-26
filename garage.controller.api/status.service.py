10c77a963717ba451a6f3e00e137200f4e089ca0
Jozef Lačný <lacny.jozef@gmail.com>
2023-07-26 23:03:06 +0200

    status.service testing on Rpi, reporting to server

--- garage.controller.api/status.service.py
+++ garage.controller.api/status.service.py
@@ -10,8 +10,8 @@ CONFIG = configparser.ConfigParser()
 CONFIG.read('config.ini')
 CLIENT = StatusApiClient(CONFIG['status.api'])
 
-POLLING_INTERVAL = int(CONFIG['status.service']['PollingInterval'])
-OPENED_SECONDS_WARNING = int(CONFIG['status.service']['OpenedSecondsWarning'])
+POLLING_INTERVAL = float(CONFIG['status.service']['PollingInterval'])
+OPENED_SECONDS_WARNING_AFTER = int(CONFIG['status.service']['OpenedSecondsWarningAfter'])
 OPENED_SECONDS_WARNING_INTERVAL = int(CONFIG['status.service']['OpenedSecondsWarningInterval'])
 HEALTH_REPORTING_INTERVAL = int(CONFIG['status.service']['HealthReportingInterval'])
 IS_CLOSED = False
@@ -21,24 +21,17 @@ DOOR_SENSOR_PIN = 11
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(DOOR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-garageTriggerTime = datetime.max
-healthReportTime = datetime.max
+garageTriggerTime = datetime.utcnow()
+healthReportTime = datetime.utcnow()
+warningReportTime = datetime.utcnow()
 
-class PiWarning:
-    def __init__(self, warning_interval):
-        self._warning_interval = warning_interval
-        self.last_warning_time = datetime.max
-
-    def print_with_debounce(self, message):
-        last_warning_diff = (datetime.utcnow() - self.last_warning_time).total_seconds()
-        if (last_warning_diff > self._warning_interval or last_warning_diff < 0):
-            self.print(message)
-            self.last_warning_time = datetime.utcnow()
-
+class PrinterWithTimestamp:
     def print(self, message):
         print(datetime.utcnow().strftime("%H:%M:%S"), message)
 
-warning = PiWarning(OPENED_SECONDS_WARNING_INTERVAL)
+p = PrinterWithTimestamp()
+
+p.print("Status service started")
 
 try:
     while True:
@@ -47,28 +40,38 @@ try:
         now = datetime.utcnow()
         lastTriggerDiff = (now - garageTriggerTime).total_seconds()
         lastHealthReportDiff = (now - healthReportTime).total_seconds()
+        warningReportDiff = (now - warningReportTime).total_seconds()
+
+        if isDoorSensorClosed is False and IS_CLOSED is True:
+            p.print("Door started opening")
+            CLIENT.set_status('OPENING')
+            garageTriggerTime = now
+            healthReportTime = now
+            IS_CLOSED = False
+            continue
+        
+        if (lastTriggerDiff > OPENED_SECONDS_WARNING_AFTER 
+            and warningReportDiff > OPENED_SECONDS_WARNING_INTERVAL
+            and isDoorSensorClosed is False):
+            p.print("Door has been opened for " + str(int(round(lastTriggerDiff))) + " seconds")
+            CLIENT.set_status('OPENED')
+            warningReportTime = now
+            healthReportTime = now
+            continue
+
+        if (lastHealthReportDiff > HEALTH_REPORTING_INTERVAL):
+            p.print("Reporting health.")
+            healthReportTime = now
+            CLIENT.report_health()
 
         if isDoorSensorClosed:
             if IS_CLOSED is False:
                 CLIENT.set_status('CLOSED')
-                warning.print("Door was just closed")
+                healthReportTime = now
+                p.print("Door was just closed")
             garageTriggerTime = datetime.max
             IS_CLOSED = True
-            continue
 
-        if isDoorSensorClosed is False and IS_CLOSED is True:
-            warning.print("Door started opening")
-            CLIENT.set_status('OPENING')
-            garageTriggerTime = now
-            IS_CLOSED = False
-            continue
-        
-        if lastTriggerDiff > OPENED_SECONDS_WARNING and isDoorSensorClosed is False:
-            warning.print_with_debounce("Door has been opened for " + str(int(round(lastTriggerDiff))) + " seconds")
-            CLIENT.set_status('OPENED')
-        
-        if (lastHealthReportDiff > HEALTH_REPORTING_INTERVAL):
-            CLIENT.report_health()
 
 except KeyboardInterrupt:
     print("Program exited")
