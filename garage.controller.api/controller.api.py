2cab010128f6d8deaa625de71a28698b310b19f0
mcsdodo <lacny.jozef@gmail.com>
2023-09-05 22:19:41 +0200

    replace controller.api with flask api + ramp open

--- garage.controller.api/controller.api.py
+++ garage.controller.api/controller.api.py
@@ -1,7 +1,6 @@
-# this runs on the PiZero in garage. Basic garage operation
+from flask import Flask, Response
+import serial
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
 import configparser
 try:
     from garagegpio import GarageGpio
@@ -10,10 +9,11 @@ except ImportError:
 from time import sleep
 
 CONFIG = configparser.ConfigParser()
-CONFIG.read('config.ini')
+CONFIG.read(['config.ini', 'secrets.ini'])
 DOOR_CLOSED_SENSOR_PIN = int(CONFIG['common']['DoorSensorClosedPin'])
 DOOR_OPENED_SENSOR_PIN = int(CONFIG['common']['DoorSensorOpenedPin'])
 RELAY_PIN = int(CONFIG['common']['RelayPin'])
+RAMP_PHONE = CONFIG['controller.api']['RampPhone']
 
 gpio = GarageGpio(DOOR_OPENED_SENSOR_PIN, DOOR_CLOSED_SENSOR_PIN, RELAY_PIN)
 
@@ -24,40 +24,40 @@ def getState():
         return 'OPENED'
     return { 'state' : 'UNKNOWN'}
 
-class MyServer(BaseHTTPRequestHandler):
-    def _set_headers(self, status, message=None):
-        self.send_response(status)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        if message is not None:
-            self.wfile.write(json.dumps(message, default=str).encode('utf-8'))
-        
-    def do_POST(self):
-        gpio.relay_on()
-        print("R1 turned on")
-        sleep(1)
-        gpio.relay_off()
-        print("R2 turned off")
-        self._set_headers(200)
-    
-    def do_GET(self):
-        self._set_headers(200, { 'state' : getState() })
+app = Flask(__name__)
 
-    def do_HEAD(self):
-        self._set_headers(200)
-    
+@app.post('/')
+def do_POST():
+    gpio.relay_on()
+    print("R1 turned on")
+    sleep(1)
+    gpio.relay_off()
+    print("R2 turned off")
+    return Response(status=200)
+
+@app.get('/')
+def do_GET():
+    return {
+        'state' : getState()
+    }
+
+@app.post('/open-ramp')
+def do_openRamp_POST():
+    ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate=115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
+    ser.write(('ATD'+RAMP_PHONE+';\r').encode())
+    ser.write('WAIT=5\r'.encode())
+    sleep(10)
+    ser.write('ATH\r'.encode())
+    ser.close()
+    return Response(status=200)
+
+
 if __name__ == "__main__":
-    HOST_NAME = "0.0.0.0"
-    SERVER_PORT = 8080
-    webServer = HTTPServer((HOST_NAME, SERVER_PORT), MyServer)
-    print("Server started http://%s:%s" % (HOST_NAME, SERVER_PORT))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        gpio.cleanup()
-
-    webServer.server_close()
-    print("Server stopped.")
+    app.run(host="0.0.0.0", port=8080)
