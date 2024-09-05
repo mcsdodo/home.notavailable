6ca45576d6f707ba332a69c892cdd001d792a132
mcsdodo <lacny.jozef@gmail.com>
2024-09-05 14:57:55 +0200

    feat: Refactor status API client to remove unnecessary headers

--- garage.controller.api/statusapiclient.py
+++ garage.controller.api/statusapiclient.py
@@ -5,18 +5,18 @@ class StatusApiClient:
         self.url = section['Url']
     def get_status(self):
         try:
-            r = requests.get(self.url, headers=self.headers, timeout=15)
+            r = requests.get(self.url, timeout=15)
             return r
         finally:
             pass
     def set_status(self, state):
         try:
-            requests.post(self.url, headers=self.headers, json ={'state': state}, timeout=15)
+            requests.post(self.url, json ={'state': state}, timeout=15)
         finally:
             pass
     def report_health(self, state = ""):
         try:
             url = self.url + state
-            requests.head(url, headers=self.headers, timeout=15)
+            requests.head(url, timeout=15)
         finally:
             pass
